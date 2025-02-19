2beb6697820ed46d6cc4efd742dbcc6e5f4ba878
Joel Júnior <joelviana250@gmail.com>
2025-02-19 16:08:46 -0300

    Feita a estilização

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const apiKey = "cdf1bd73a062caed520049b6103ed3af";
+const form = document.getElementById("form");
+const city = document.getElementById("inputCityName");
+const cityName = document.querySelector('.city__name')
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(city.value);
+  cityName.classList.toggle('hidden')
+});
 
 async function getWeatherData(city) {
   const weatherData = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=standard&appid=${apiKey}&lang=pt_br`;
