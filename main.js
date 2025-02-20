407a9d22abaf961be99ec78a967b10efdd987486
Joel Júnior <joelviana250@gmail.com>
2025-02-20 15:46:05 -0300

    Adicionada responsividade ao app

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const apiKey = "cdf1bd73a062caed520049b6103ed3af";
 const form = document.querySelector(".container__inputs");
 const typedCity = document.getElementById("typedCityInput");
+const paragraphError = document.querySelector(".city__error");
 const cityName = document.querySelector(".city__name");
 const containerTemp = document.querySelector(".container__temp");
 const ulAdditionalInfos = document.querySelector(".additional__infos");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  showWeatherData();
-});
+const body = document.querySelector("body");
 
 async function getWeatherData(city) {
   try {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${apiKey}`;
     const res = await fetch(apiUrl).then((res) => res.json());
-    console.log(res);
+    if (typedCity.value == "") {
+      paragraphError.textContent = `Por favor digite o nome de uma cidade!`;
+      paragraphError.classList.remove("hidden");
+    } else if (res.cod != 200) {
+      paragraphError.textContent = `A cidade não foi encontrada!`;
+      paragraphError.classList.remove("hidden");
+    } else {
+      paragraphError.classList.add("hidden");
+    }
     return res;
   } catch (error) {
     console.error(`Foi obtido o seguinte erro: ${error}`);
@@ -21,13 +27,16 @@ async function getWeatherData(city) {
 }
 
 function setWeatherIcons(id) {
-  const iconImage = document.querySelector('.descriptionImage');
-  iconImage.setAttribute('src', `https://openweathermap.org/img/wn/${id}@2x.png`)
+  const iconImage = document.querySelector(".descriptionImage");
+  iconImage.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${id}@2x.png`
+  );
 }
 
 async function showWeatherData() {
   const data = await getWeatherData(typedCity.value);
-  setWeatherIcons(data.weather[0].icon)
+  setWeatherIcons(data.weather[0].icon);
   const temp = document.querySelector(".temp");
   const tempDescription = document.querySelector(".temp__description");
   const tempDescriptionText = data.weather[0].description;
@@ -49,3 +58,17 @@ async function showWeatherData() {
   wind.textContent = `${data.wind.speed}km/h`;
   typedCity.value = "";
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  showWeatherData();
+});
+
+body.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target == body) {
+    cityName.classList.toggle("hidden");
+    containerTemp.classList.toggle("hidden");
+    ulAdditionalInfos.classList.toggle("hidden");
+  }
+});
